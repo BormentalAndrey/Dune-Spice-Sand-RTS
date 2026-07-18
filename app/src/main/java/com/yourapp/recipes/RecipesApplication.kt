942f76800dadef8dc08492568715c18d7684eb26
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-18 21:07:38 +0300

    Create RecipesApplication.kt

---
+++ app/src/main/java/com/yourapp/recipes/RecipesApplication.kt
@@ -0,0 +1,9 @@
+// app/src/main/java/com/yourapp/recipes/RecipesApplication.kt
+
+package com.yourapp.recipes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RecipesApplication : Application()
