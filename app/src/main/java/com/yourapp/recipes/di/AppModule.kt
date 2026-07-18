52a72b40ecef05e66206a636227354a7c86ae2d9
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-19 00:34:10 +0300

    Update AppModule.kt

--- app/src/main/java/com/yourapp/recipes/di/AppModule.kt
+++ app/src/main/java/com/yourapp/recipes/di/AppModule.kt
@@ -2,10 +2,14 @@ package com.yourapp.recipes.di
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.yourapp.recipes.data.repository.MealPlanRepositoryImpl
 import com.yourapp.recipes.data.repository.RecipeRepositoryImpl
 import com.yourapp.recipes.data.repository.ShoppingListRepositoryImpl
+import com.yourapp.recipes.data.repository.UserPreferencesRepositoryImpl
+import com.yourapp.recipes.domain.repository.MealPlanRepository
 import com.yourapp.recipes.domain.repository.RecipeRepository
 import com.yourapp.recipes.domain.repository.ShoppingListRepository
+import com.yourapp.recipes.domain.repository.UserPreferencesRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,4 +39,16 @@ object AppModule {
     fun provideShoppingListRepository(
         repository: ShoppingListRepositoryImpl
     ): ShoppingListRepository = repository
+    
+    @Provides
+    @Singleton
+    fun provideMealPlanRepository(
+        repository: MealPlanRepositoryImpl
+    ): MealPlanRepository = repository
+    
+    @Provides
+    @Singleton
+    fun provideUserPreferencesRepository(
+        repository: UserPreferencesRepositoryImpl
+    ): UserPreferencesRepository = repository
 }
