f8c8995e4cebac483ecf3d672bd68ec21cc2f077
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-19 00:08:26 +0300

    Update GetFilteredRecipesUseCase.kt

--- app/src/main/java/com/yourapp/recipes/domain/usecase/recipe/GetFilteredRecipesUseCase.kt
+++ app/src/main/java/com/yourapp/recipes/domain/usecase/recipe/GetFilteredRecipesUseCase.kt
@@ -6,10 +6,6 @@ import com.yourapp.recipes.domain.repository.RecipeRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-/**
- * Use case для получения отфильтрованного списка рецептов.
- * Инкапсулирует бизнес-логику фильтрации и сортировки.
- */
 class GetFilteredRecipesUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
 ) {
