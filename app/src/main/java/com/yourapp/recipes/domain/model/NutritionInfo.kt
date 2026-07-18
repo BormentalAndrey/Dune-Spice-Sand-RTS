c2845bb099fc87d60ee2cef943a48b0ee0dc46f8
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-18 22:48:12 +0300

    Update NutritionInfo.kt

--- app/src/main/java/com/yourapp/recipes/domain/model/NutritionInfo.kt
+++ app/src/main/java/com/yourapp/recipes/domain/model/NutritionInfo.kt
@@ -1,6 +1,8 @@
+package com.yourapp.recipes.domain.model
+
 data class NutritionInfo(
     val calories: Float = 0f,
-    val proteins: Float = 0f, // граммы
-    val fats: Float = 0f, // граммы
-    val carbohydrates: Float = 0f // граммы
+    val proteins: Float = 0f,
+    val fats: Float = 0f,
+    val carbohydrates: Float = 0f
 )
