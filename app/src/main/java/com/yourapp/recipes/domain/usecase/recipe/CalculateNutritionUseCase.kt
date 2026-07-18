c79ec331d03ea90e08e1461f14070062b7e83a08
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-18 21:01:15 +0300

    Create CalculateNutritionUseCase.kt

---
+++ app/src/main/java/com/yourapp/recipes/domain/usecase/recipe/CalculateNutritionUseCase.kt
@@ -0,0 +1,19 @@
+package com.yourapp.recipes.domain.usecase.recipe
+
+import com.yourapp.recipes.domain.model.NutritionInfo
+import javax.inject.Inject
+
+/**
+ * Use case для пересчета КБЖУ при изменении количества порций.
+ */
+class CalculateNutritionUseCase @Inject constructor() {
+    
+    operator fun invoke(baseNutrition: NutritionInfo, multiplier: Float): NutritionInfo {
+        return NutritionInfo(
+            calories = baseNutrition.calories * multiplier,
+            proteins = baseNutrition.proteins * multiplier,
+            fats = baseNutrition.fats * multiplier,
+            carbohydrates = baseNutrition.carbohydrates * multiplier
+        )
+    }
+}
