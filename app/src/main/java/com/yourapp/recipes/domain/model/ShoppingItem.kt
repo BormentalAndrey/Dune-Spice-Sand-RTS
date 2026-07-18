b5ff6d675bcb40af2f6a81253fc8d9a7ceb15253
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-07-18 21:22:56 +0300

    Create ShoppingItem.kt

---
+++ app/src/main/java/com/yourapp/recipes/domain/model/ShoppingItem.kt
@@ -0,0 +1,21 @@
+package com.yourapp.recipes.domain.model
+
+data class ShoppingItem(
+    val id: Long = 0,
+    val name: String,
+    val quantity: Float,
+    val unit: String,
+    val category: String = "other",
+    val isPurchased: Boolean = false,
+    val recipeId: Long? = null
+) {
+    val displayQuantity: String
+        get() = if (quantity == quantity.toLong().toFloat()) {
+            quantity.toLong().toString()
+        } else {
+            String.format("%.1f", quantity)
+        }
+    
+    val displayText: String
+        get() = "$displayQuantity $unit $name"
+}
